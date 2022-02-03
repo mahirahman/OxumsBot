34b0fcabfc297c5560d1d4162c702036174fa725
Mahi <starexify@gmail.com>
2022-02-03 18:03:19 +1100

    Fixed timezone bug
    - Added compatibility to the current time zone bot is running from (AEST)

--- commands/force.js
+++ commands/force.js
@@ -10,15 +10,19 @@ module.exports = {
             return message.reply(`This command does not take any arguments. Please use !force to check when the next force wipe is.`);
         }
 
-        // The force time is 11 AM PST or 5 AM AEST etc
+        // The force time is 11 AM PST or 7:00 pm GMT or 6 AM AEST etc
         // This value needs to be hardcoded based on the location of the system this bot is running on
-        FORCE_TIME = '6:0:00';
-        timeUntilForce = dhms(moment(FORCE_TIME, 'HH:mm:ss').diff(moment(getCurrentTime(), 'HH:mm:ss')));
+        // Extra day is added due to AEST being a day ahead
+        const FORCETIME = 6;
+        forceWipe = getForceWipe(FORCETIME).setDate(getForceWipe().getDate() + 1);
+
+        currTime = Date.now();
+        timeUntilForce = dhm(forceWipe - currTime);
 
         const embed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .addFields(
-            { name: 'Next Force Wipe', value: `\`\`\`${moment(getForceWipe()).format("MMMM Do")}\`\`\``},
+            { name: 'Next Force Wipe', value: `\`\`\`${moment(getForceWipe()).format("MMMM Do")} GMT\`\`\``},
             { name: 'Time Until Force Wipe', value: `\`\`\`${timeUntilForce}\`\`\``}
         )
         return message.channel.send({embeds: [embed]});
@@ -26,7 +30,7 @@ module.exports = {
 };
 
 // Returns the date of the next force wipe
-function getForceWipe() {
+function getForceWipe(forceTime=0) {
     const date = new Date();
     let currDate = date.getDate();
     let currMonth = date.getMonth();
@@ -34,35 +38,39 @@ function getForceWipe() {
     // Check if current date is within the first week of the current month
     if (currDate <= 7) {
         // First thursday of the current month
-        const firstThursday = getFirstThursdayDate(currMonth);
+        const firstThursday = getFirstThursdayDate(currMonth, forceTime);
         if (currDate <= firstThursday) {
             return firstThursday;
         }
     }
-    return getFirstThursdayDate(currMonth === 11 ? currMonth = 0 : currMonth += 1);
+    return getFirstThursdayDate(currMonth === 11 ? currMonth = 0 : currMonth = currMonth + 1, forceTime);
 };
 
 // Given a month, finds the first thursday of that month
-function getFirstThursdayDate(month) {
+function getFirstThursdayDate(month, forceTime) {
     const date = new Date();
-
     // Set date to first day of the given month
     date.setMonth(month);
     date.setDate(1);
+
+    // Initialise the time
+    date.setHours(forceTime);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
 
     // Find the first thursday of the month
     while (date.getDay() !== 4) {
         date.setDate(date.getDate() + 1);
     }
+    // This is due to AEST being a day ahead
+    //date.setDate(date.getDate() + 1);
     return date;
 };
 
-function getCurrentTime() {
-    date = new Date();
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-};
-
-function dhms(ms) {
+// Convert milliseconds to days, hours, minutes and seconds
+// Source: https://stackoverflow.com/questions/8528382/javascript-show-milliseconds-as-dayshoursmins-without-seconds
+function dhm(ms) {
     const days = Math.floor(ms / (24*60*60*1000));
     const daysms = ms % (24*60*60*1000);
     const hours = Math.floor(daysms / (60*60*1000));
@@ -70,5 +78,6 @@ function dhms(ms) {
     const minutes = Math.floor(hoursms / (60*1000));
     const minutesms = ms % (60*1000);
     const sec = Math.floor(minutesms / 1000);
-    return `${1+days}d | ${hours}h | ${minutes}m | ${sec}s`;
+
+    return `${days}d | ${hours}h | ${minutes}m | ${sec}s`;
 };
